e6e4cd8677de7b6169da32da6e777b31ac834d8b
Subin <70842920+bin-pro@users.noreply.github.com>
2023-10-11 14:14:31 +0900

    Init: Initialize for gateway service

---
+++ src/main/java/com/example/legendfive/PdaGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.legendfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdaGatewayServiceApplication.class, args);
+	}
+
+}
